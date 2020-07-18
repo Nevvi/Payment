a4a801651e6b4187fafb369b8d406ee1ed8f87f2
Tyler Cobb <tyjcobb94@gmail.com>
2020-07-18 07:03:20 -0500

    Added braintree and deleted copied code

--- src/error/Errors.js
+++ src/error/Errors.js
@@ -6,17 +6,3 @@ module.exports.InvalidRequestError = class extends Error {
         this.statusCode = 400;
     }
 }
-
-module.exports.UserNotFoundError = class extends Error {
-    constructor(userId) {
-        super(`User not found with id: ${userId}`)
-        this.statusCode = 404;
-    }
-}
-
-module.exports.UserAlreadyExistsError = class extends Error {
-    constructor(userId) {
-        super(`User already exists with id: ${userId}`)
-        this.statusCode = 409;
-    }
-}
